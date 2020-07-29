aa91110d2cf2730bf95d54cc6a11ebb844a8bb0a
lgm527 <laurellmccaffrey@gmail.com>
2020-07-29 14:42:00 -0400

    front and back of card basics

--- src/components/Back.js
+++ src/components/Back.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-function Back() {
-
-  return (
-    <div className="Back">
-        Back
-    </div>
-  );
+function Back(props) {
+    console.log(props.user);
+    const { bio, blog, location, public_gists, public_repos } = props.user;
+    return (
+      <div className="Back">
+          <p>{bio}</p>
+          <a href={blog}>{blog}</a>
+          <p>Coding in <i>{location}</i></p>
+          <p>Repos ............................ {public_repos}</p>
+          <p>Gists ................................. {public_gists}</p>
+      </div>
+    );
 }
 
 export default Back;
