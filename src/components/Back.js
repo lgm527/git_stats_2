26f7e3621511e3e710a8fed6c0f04b348993664b
lgm527 <laurellmccaffrey@gmail.com>
2020-07-30 12:27:40 -0400

    count days user has made github account

--- src/components/Back.js
+++ src/components/Back.js
@@ -3,7 +3,18 @@ import '../assets/Back.css';
 
 function Back(props) {
     console.log(props.user);
-    const { bio, blog, location, public_gists, public_repos, html_url, name } = props.user;
+    const { bio, blog, location, public_gists, public_repos, html_url, name, created_at } = props.user;
+
+    const dayCount = () => {
+      const oneDay = 24 * 60 * 60 * 1000;
+      const since = new Date(created_at);
+      const now = new Date();
+
+      const days = Math.round(Math.abs((since - now) / oneDay));
+
+      return days;
+    }
+
     return (
       <div className="Back">
           <h6>{name}</h6>
@@ -13,6 +24,7 @@ function Back(props) {
           <p>Coding in <i>{location}</i></p>
           <p>Repos ............................ {public_repos}</p>
           <p>Gists ................................. {public_gists}</p>
+          <p>Days .............................. {dayCount()}</p>
       </div>
     );
 }
