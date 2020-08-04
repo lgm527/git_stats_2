fec01cee3375c6de206b5ea52f7cfd570197ea1d
lgm527 <laurellmccaffrey@gmail.com>
2020-08-04 12:18:13 -0400

    form added

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../assets/App.css';
 import Card from './Card';
 
 function App() {
 
-  // fetch loading status
-  const [isLoaded, setIsLoaded] = useState(false);
-  // fetch catch errors
-  const [error, setError] = useState(null);
-  // save fetched data
-  const [user, setUser] = useState([]);
+    // username input default blank
+    const [username, setUsername] = useState('');
+  
+    // controlled form
+    const handleUsernameChange = (e) => setUsername(e.target.value);
 
-  // useEffect(() => {
-  //     fetch(`https://api.github.com/users/${user}`, {
-  //         method: 'GET',
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           "Accept": "application/vnd.github.v3+json",
-  //           "User-Agent": "lgm527",
-  //           "Authorization": "token " + process.env.REACT_APP_GH_TOKEN,
-  //         }
-  //     })
-  //     .then(res => res.json())
-  //     .then((user) => {
-  //         setIsLoaded(true);
-  //         setUser(user);
-  //     },
-  //     (error) => {
-  //         setIsLoaded(true);
-  //         setError(error);
-  //     })
-  // }, [])
 
-  // if (error) {
-  //     return <div>Error: {error.message}</div>
-  // } else if (!isLoaded) {
-  //     return <div>Loading...</div>
-  // } else { ...return statement }
+    // fetch loading status
+    const [isLoaded, setIsLoaded] = useState(false);
+    // fetch catch errors
+    const [error, setError] = useState(null);
+    // save fetched data
+    const [user, setUser] = useState([]);
+
+    const fetchUser = () => {
+      fetch(`https://api.github.com/users/${username}`, {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/vnd.github.v3+json",
+          "User-Agent": "lgm527",
+          "Authorization": "token " + process.env.REACT_APP_GH_TOKEN,
+        }
+      })
+      .then(res => res.json())
+      .then((user) => {
+          setIsLoaded(true);
+          setUser(user);
+          console.log(user);
+      })
+      .catch(error => {
+          setIsLoaded(true);
+          setError(error);
+      })
+    }
 
   return (
     <div className="App">
       
       <form>
             <label htmlFor='user'>User: </label>
-            <input type='text' name='user' id='user' />
-            <input type='submit' value='Submit' />
+            <input 
+              type='text'
+              name='username'
+              id='username'
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            <input type='button' value='Swing' onClick={fetchUser} />
       </form>
       
       {/* <Card /> */}
