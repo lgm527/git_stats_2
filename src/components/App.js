5326419656057cde8e86ce5c35373abc20f636c9
lgm527 <laurellmccaffrey@gmail.com>
2020-08-04 13:34:35 -0400

    clear form upon submission

--- src/components/App.js
+++ src/components/App.js
@@ -29,6 +29,7 @@ function App() {
       .then((user) => {
           setIsLoaded(true);
           setUser(user);
+          setUsername('');
       })
       .catch(error => {
           setIsLoaded(true);
