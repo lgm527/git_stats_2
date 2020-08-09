0be9560cdeaf4a8eed1a3479f67fd64ab94489b6
lgm527 <laurellmccaffrey@gmail.com>
2020-08-09 19:58:24 -0400

    flip card with keyboard

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,8 +6,20 @@ import Back from './Back';
 function Card(props) {
   // flip card view on click
   const [front, setFront] = useState(true);
+
+  const handleKeyDown = (event) => {
+    if(event.key === 'Enter' || event.key === ' ') {
+      setFront(!front);
+    }
+  }
+
     return (
-        <div className='Card' onClick={() => {front ? setFront(false) : setFront(true)}}>
+        <div 
+        className='Card' 
+        onClick={() => {front ? setFront(false) : setFront(true)}} 
+        tabIndex='0'
+        onKeyDown={(event) => handleKeyDown(event)}
+        >
             {front ? <Front user={props.user} /> : <Back user={props.user}/>}
         </div>
       );
